5e94f60c36adab21c08e38870aef896ce5af4dbe
Angie Mugo <Angie@Angies-MacBook-Pro.local>
2016-12-08 20:42:19 +0300

    removes unnecessary tests

--- app/room.py
+++ app/room.py
@@ -1,19 +1,19 @@
-class RoomClass:
+class RoomClass(object):
 
-    def __init__(self, room_name, room_type, max_occupants):
+    def __init__(self, room_name=None, room_type=None, max_occupants=None):
         self.room_name = room_name
         self.room_type = room_type
         self.max_occupants = max_occupants
 
 
 class Office:
-    def __init__(self, room_name, max_occupants):
-        super(Office, self).__init__(room_name, max_occupants=6)
-
+    def __init__(self, room_name, room_type):
+        # super.__init__(room_name, room_type, max_occupants=6)
+        super(Office, self).__init__(room_name, room_type, max_occupants=6)
 
 class Lspace:
-    def __init__(self, room_name, max_occupants):
+    def __init__(self, room_name, room_type):
 
-        super(Livingspace, self).__init__(room_name, max_occupants=4)
+        # super.__init__(room_name, room_type, max_occupants=4)
+        super(Lspace, self).__init__(room_name, room_type, max_occupants=4)
 
-        self.room_name = room_name
---
+++ tests/test_amity_failure.py
@@ -0,0 +1,5 @@
+import unittest
+import App.Amity
+
+class ClassAmityTestFailure(unittest.TestCase):
+    def test_if_input_is_not_string(self):
